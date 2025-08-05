c94c362ce6934cacd4b1ba7fc81e73dc37d18ed5
LoLawliet <stivsasha@gmail.com>
2025-08-05 15:49:19 +0300

    Сделанная Практическая Работа №1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,39 @@
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        Car[] cars = new Car[3];
+
+        for (int i = 0; i < 3; i++){
+            System.out.println("Введите данные для автомобиля " + (i + 1) + ":");
+
+            System.out.println("Название: ");
+            String name = scanner.nextLine();
+
+
+            boolean validSpeed = false;
+            while (!validSpeed){
+
+                System.out.println("Введите скорость от 1 до 250 км/ч: ");
+                String scanner1 = scanner.nextLine().trim();
+                int speed = Integer.parseInt(scanner1);
+                if (speed > 0 && speed < 250){
+                    validSpeed = true;
+                    cars[i] = new Car(name, speed);
+                    break;
+                }else {
+                    System.out.println("Скорость должна быть от 1 до 250 км/ч. Попробуйте еще раз. ");
+                }
+            }
+
+        }
+        Race race = new Race(cars);
+        Car leader = race.getLeader();
+
+        System.out.println("Победититель: " + leader.getName());
+
     }
+
 }
